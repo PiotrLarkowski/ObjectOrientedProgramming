d91fa23c5b58f77e859ffb31dd8a30400c5a861c
PiotrLarkowski <p.larkowski90@gmail.com>
2023-08-08 19:19:34 +0200

    Encapsulation example

--- main.cpp
+++ main.cpp
@@ -3,17 +3,44 @@ using namespace std;
 using std::string;
 
 class Employee{
-public:
+private:
     string Name;
     string Company;
     int Age;
+public:
     Employee(string name, string company, int age){
-        Age = age;
-        Name = name;
-        Company = company;
+            Age = age;
+            Name = name;
+            Company = company;
     }
     void IntroduceYourSelf(){
         cout<<"My name is " << Name <<" I'm working for "<<Company<< " and I have " <<Age << " years old"<<endl;
+    }
+
+    const string &getName() const {
+        return Name;
+    }
+
+    void setName(const string &name) {
+        Name = name;
+    }
+
+    const string &getCompany() const {
+        return Company;
+    }
+
+    void setCompany(const string &company) {
+        Company = company;
+    }
+
+    int getAge() const {
+        return Age;
+    }
+
+    void setAge(int age) {
+        if(age >=18) {
+            Age = age;
+        }
     }
 };
 
@@ -22,6 +49,12 @@ int main() {
     Employee employee2 = Employee("Eric","Ditrion",27);
     employee1.IntroduceYourSelf();
     employee2.IntroduceYourSelf();
+    employee1.setName("Tom");
+    employee1.setAge(31);
+    employee1.IntroduceYourSelf();
+    cout<<employee1.getName() <<" is " << employee1.getAge() << " years old";
+    employee1.setAge(15);
+    employee1.IntroduceYourSelf();
     system("pause>0");
     return 0;
 }
